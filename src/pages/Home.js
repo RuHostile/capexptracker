2fc603d61a2bc92e6f70ec77fefef70c6e87d08c
RuHostile <djb3501@gmail.com>
2023-01-11 12:44:51 +0000

    added css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,25 @@ import "../App.css";
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import Login from "./Login";
+import Projects from '../data/projects.json';
+import Button from "react-bootstrap/Button";
+
+function getProjects() {
+    Projects && Projects.map( project => {
+              return (
+                <div className="projectBox" key={project.ID}>
+                  <strong>{project.Name}</strong> <br />
+                  { project.Description }<br />
+                  { project.Department }<br />
+                  capital expenditure : { project["Capital Expenditure"] }<br /> 
+                </div>
+              )
+            } )
+}
 
 function Home() {
+
+  
   const [logout, setLogout] = React.useState(false);
 
   if (logout) {
@@ -11,8 +28,41 @@ function Home() {
   }
   return (
     <div>
-      <h1>Home Page</h1>
-      <button onClick={() => setLogout(true)}>Logout</button>
+      <div className="v-navbar">
+        <ul>
+          <h1>Capital Expenditure Tracker</h1>
+          <li><a className="active" herf="#home">Home</a></li>
+          <li><a href="#">Description</a></li>
+          <li><a href="/">Logout</a></li>
+          <li><Button href="/">logout</Button></li>
+
+        </ul>
+      </div>
+      <div className="main-box">
+        <h1>Home Page</h1>
+        <div className="main-box-button">
+          <button>new</button>
+          <getProjects></getProjects>
+        </div>
+        
+        <div className="projectBox-container">
+
+          {
+            Projects && Projects.map( project => {
+              return (
+                <div className="projectBox" key={project.ID}>
+                  <strong>{project.Name}</strong> <br />
+                  { project.Description }<br />
+                  { project.Department }<br />
+                  capital expenditure : { project["Capital Expenditure"] }<br /> 
+                </div>
+              )
+            } )
+          }
+        </div>      
+      </div>
+      <getProjects/>
+      
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,9 +2,43 @@ import "../App.css";
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import Login from "./Login";
+import  Form  from "react-bootstrap/Form";
+import Users from '../data/users.json';
 
-function Register() {
+function Register(props) {
   const [goToLogin, setGoToLogin] = React.useState(false);
+
+  //set user data 
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const changeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+  const changeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const changePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const transferValue = (event) => {
+    event.preventDefault();
+    const val = {
+      username,
+      email,
+      password,
+    }
+    props.func(val);
+    clearState();
+  };
+
+  const clearState = () => {
+    setUsername('');
+    setEmail('');
+    setPassword('');
+  };
 
   if (goToLogin) {
     return <Navigate to="/" />;
@@ -13,7 +47,38 @@ function Register() {
   return (
     <div>
       <h1>Register Your Account</h1>
-      <button onClick={() => setGoToLogin(true)}> Register </button>
+      <div className="container-register">
+        <Form>
+          <Form.Group>
+            <Form.Label>Enter Username</Form.Label>
+            <Form.Control></Form.Control>
+            <Form.Text></Form.Text>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Enter Email</Form.Label>
+            <Form.Control></Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Re-Enter Email</Form.Label>
+            <Form.Control></Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Enter Password</Form.Label>
+            <Form.Control></Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Re-Enter Password</Form.Label>
+            <Form.Control></Form.Control>
+          </Form.Group>
+
+        </Form>
+      </div>
+      <button onClick={transferValue}>Save</button>
+      <button onClick={() => setGoToLogin(true)}> Go Back </button>
     </div>
   );
 }
